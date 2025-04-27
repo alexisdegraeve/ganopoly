54ee99ce0cd3f194dd44fd84c1508be222f1d0ed
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-04-27 15:03:34 +0200

    add game add page not found

--- ganopoly/src/app/app.routes.ts
+++ ganopoly/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/game', pathMatch: 'full' },  // Redirection vers "game" par défaut
+  { path: 'game', component: GameComponent },            // Route vers le composant "Game"
+  { path: '**', component: PageNotFoundComponent }       // Page 404 si la route n'est pas définie
+];
+
