c821221d84b7bf00ba0ebba393e06bb2aeadeaf0
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-05-04 20:50:55 +0200

    style bank note

--- ganopoly/src/app/banknote/banknote.component.ts
+++ ganopoly/src/app/banknote/banknote.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'gano-banknote',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './banknote.component.scss'
 })
 export class BanknoteComponent {
-
+  @Input() color: string = 'white';
+  @Input() value: number = 1;
 }
