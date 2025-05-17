631cbaddb8053ee61a47a7342173b6eed54c1256
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-05-17 19:16:51 +0200

    change board

--- ganopoly/src/app/board/board.component.ts
+++ ganopoly/src/app/board/board.component.ts
@@ -2,15 +2,22 @@ import { Component, Input } from '@angular/core';
 import { CellComponent } from "../cell/cell.component";
 import { CommonModule } from '@angular/common';
 import { GameService } from '../services/game.service';
-import { map } from 'rxjs/operators';
+import { combineLatestWith, map, startWith } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { CardType, Case } from '../models/card';
 import { ChanceCardComponent } from "../chance-card/chance-card.component";
 import { CommunityCardComponent } from "../community-card/community-card.component";
 
+interface BoardData {
+  top: Case[];
+  left: Case[];
+  right: Case[];
+  bottom: Case[];
+}
+
 @Component({
   selector: 'gano-board',
-  imports: [CellComponent, CommonModule, ChanceCardComponent, CommunityCardComponent],
+  imports: [CellComponent, CommonModule, ChanceCardComponent, CommunityCardComponent, CommonModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
@@ -20,12 +27,36 @@ export class BoardComponent {
   leftCards$: Observable<Case[]>;
   rightCards$: Observable<Case[]>;
   bottomCards$: Observable<Case[]>;
+  isLoading$: Observable<boolean>;
+  boardData$: Observable<BoardData>;
+
 
   constructor(private gameService: GameService) {
     this.topCards$ = this.getCardsInRange(20, 30, 'vertical');
     this.leftCards$ = this.getCardsInRange(11, 19, 'horizontal');
     this.rightCards$ = this.getCardsInRange(31, 39, 'horizontal');
     this.bottomCards$ = this.getCardsInRange(0, 10, 'vertical');
+    this.boardData$ = this.topCards$.pipe(
+      combineLatestWith(
+        this.leftCards$,
+        this.rightCards$,
+        this.bottomCards$
+      ),
+      map(([top, left, right, bottom]) => ({
+        top,
+        left,
+        right,
+        bottom
+      }))
+    );
+
+    this.isLoading$ = this.boardData$.pipe(
+      map(({ top, left, right, bottom }) =>
+        !top.length || !left.length || !right.length || !bottom.length
+      ),
+      startWith(true)
+    );
+
   }
 
   private getCardsInRange(
